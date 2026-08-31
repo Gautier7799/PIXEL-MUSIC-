28c32caaa40cb6973dac512ef26c1b8e1a202d29
GAIDI AMJED <150559809+Gautier7799@users.noreply.github.com>
2026-08-31 13:47:03 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/pixelmusic/MainActivity.kt
+++ app/src/main/java/com/example/pixelmusic/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.pixelmusic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.pixelmusic.ui.AppNavigation // تم التعديل هنا
+import com.example.pixelmusic.ui.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         setContent {
-            AppNavigation() // تم التعديل هنا
+            // الآن التطبيق يبدأ من خريطة التوجيه بدلاً من شاشة واحدة ثابتة
+            AppNavigation() 
         }
     }
 }
